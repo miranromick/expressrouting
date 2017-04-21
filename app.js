0479baf8846370b4b4a262964c91d0ece3163a4d
miranromick <miranman@hotmail.com>
2017-04-21 16:35:58 -0700

    v3

--- app.js
+++ app.js
@@ -1,37 +1,89 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
+app.use(express.static('public'))  //tell Express that well keep files in the /public directory
+app.set('view engine', 'ejs')
+
+// qeustions for trivia
+var questions = {
+  'coronado-bridge':{
+    question: "Who was the first person to ever drive over the Coronado bridge?",
+    answer: "Ronald Reagan",
+    img: <img src ='images/coronadobridge.jpg'>
+  },
+  'hotel-del':{
+    question: "What is the largest wooden structure in the United States?  (Hint, its located in San Diego)",
+    answer: "Hotel Del Coronado"
+  },
+  'san-diego-county-fair':{
+    question: "What was the original name of the San Diego County Fair?",
+    answer: "Del Mar Fair"
+  },
+  'mission-bay':{
+    question: "How many visitors come to Mission Bay Park every year?",
+    answer: "More than 5 million"
+  },
+  'la-jolla-playhouse':{
+    question: "What famous actor founded the La Jolla Playhouse?",
+    answer: "Gregory Peck"
+  }
+};
+
+app.get('/trivia/:question', function(request, response){
+  var questionKey = request.params.question
+  var triviaQuestion = questions[questionKey]
+  response.render('trivia', {'question': triviaQuestion.question, 'answer': triviaQuestion.answer, 'img': triviaQuestion.img})
+})
+
+app.get('/', function (request, response) {
+  response.render('index', {'allQuestions': questions});
+});
 
 function reverse(s){
-  return s.split("").reverse().join("");
+  return s.split("").reverse().join("")
 }
  function concat(string1, string2){
    returnstring1 + string2
  }
 
+app.get('/', function (request, response) {
+ response.render("index")
+})
+
 app.get('/home', function (request, response) {
- response.send('Hello World!');
-});
+ response.send('Hello World! Enjoy yourself')
+})
 
 app.get('/hello/:name', function (request, response){
-  var name = request.params["name"];
-  response.send("Hello " + name + ".  Welcome!");
-});
+  var name = request.params["name"]
+  response.send("Hello " + name + ".  Welcome!")
+})
 
 app.get('/reverse/:name', function (request, response){
   var name = request.params["name"]
   response.send("reverse order "  + reverse(name))
-});
+})
 
 app.get('/add/:string1/:string2', function (request, response){
   var string1 = request.params["string1"]
   var string2 = request.params["string2"]
   response.send("add together " + concat(string1, string2))
-});
+})
 
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!');
-// });
+app.get('/add/:string1/:string2', function (request, response){
+  var string1 = request.params["string1"]
+  var string2 = request.params["string2"]
+  response.send("add together " + concat(string1, string2))
+})
 
-app.listen(process.argv[2], function () {
-  console.log('Example app listening on port 4000!');
-});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
+
+
+
+
+
+// app.listen(process.argv[2], function () {
+//   console.log('Example app listening on port 4000!')
+// })
---
+++ trivia/question.js
@@ -0,0 +1,25 @@
+// extra page
+
+
+// var questions = {
+//   'coronado-bridge':{
+//     question: "Who was the first person to ever drive over the Coronado bridge?",
+//     answer: "Ronald Reagan"
+//   },
+//   'hotel-del':{
+//     question: "What is the largest wooden structure in the United States?  (Hint, its located in San Diego)",
+//     answer: "Hotel Del Coronado"
+//   },
+//   'san-diego-county-fair':{
+//     question: "What was the original name of the San Diego County Fair?",
+//     answer: "Del Mar Fair"
+//   },
+//   'mission-bay':{
+//     question: "How many visitors come to Mission Bay Park every year?",
+//     answer: "More than 5 million"
+//   },
+//   'la-jolla-playhouse':{
+//     question: "What famous actor founded the La Jolla Playhouse?",
+//     answer: "Gregory Peck"
+//   }
+// };
